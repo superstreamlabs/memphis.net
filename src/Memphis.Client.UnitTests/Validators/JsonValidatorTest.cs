40ebc5bfecd19161d85051b63384219169c91461
Bazen <bazenteklehaymanot29@gmail.com>
2022-12-29 14:40:51 +0300

    Added JsonValidator tests

---
+++ src/Memphis.Client.UnitTests/Validators/JsonValidatorTest.cs
@@ -0,0 +1,77 @@
+using System.Collections.Concurrent;
+using System.Text;
+using GraphQL.Types;
+using Memphis.Client.Exception;
+using Memphis.Client.UnitTests.Validators.TestData;
+using Memphis.Client.Validators;
+using NJsonSchema;
+using Xunit;
+
+namespace Memphis.Client.UnitTests.Validators
+{
+    public class JsonValidatorTest
+    {
+        private readonly ISchemaValidator _sut;
+
+        public JsonValidatorTest()
+        {
+            _sut = new JsonValidator();
+        }
+
+        #region JsonValidatorTest.ParseAndStore
+        [Theory]
+        [MemberData(nameof(JsonValidatorTestData.ValidSchema),MemberType = typeof(JsonValidatorTestData))]
+        public void ShouldReturnTrue_WhenParseAndStore_WhereValidSchemaPassed(string validSchema)
+        {
+            var actual = _sut.ParseAndStore("test-schema-001", validSchema);
+
+            Assert.True(actual);
+        }
+
+
+        [Theory]
+        [MemberData(nameof(JsonValidatorTestData.InvalidSchema), MemberType = typeof(JsonValidatorTestData))]
+        public void ShouldReturnFalse_WhenParseAndStore_WhereInValidSchemaPassed(string invalidSchema)
+        {
+            var actual = _sut.ParseAndStore("test-schema-001", invalidSchema);
+
+            Assert.False(actual);
+        }
+
+        #endregion
+
+
+        #region JsonValidatorTest.ValidateAsync
+
+        [Theory]
+        [MemberData(nameof(JsonValidatorTestData.ValidSchemaDetail), MemberType = typeof(JsonValidatorTestData))]
+        public async Task ShouldDoSuccess_WhenValidateAsync_WhereValidDataPassed(string schemaKey,string schema, byte[] msg)
+        {
+            _sut.ParseAndStore(schemaKey, schema);
+
+            await _sut.ValidateAsync(msg, schemaKey);
+        }
+
+        [Theory]
+        [MemberData(nameof(JsonValidatorTestData.InvalidSchemaDetail), MemberType = typeof(JsonValidatorTestData))]
+        public async Task ShouldDoThrow_WhenValidateAsync_WhereInvalidDataPassed(string schemaKey,string schema, byte[] msg)
+        {
+            _sut.ParseAndStore(schemaKey, schema);
+
+             await Assert.ThrowsAsync<MemphisSchemaValidationException>(
+                async () => await _sut.ValidateAsync(msg, schemaKey));
+        }
+
+        [Theory]
+        [MemberData(nameof(JsonValidatorTestData.Message), MemberType = typeof(JsonValidatorTestData))]
+        public async Task ShouldDoThrow_WhenValidateAsync_WhereSchemaNotFoundInCache(byte[] msg)
+        {
+            var nonexistentSchema = Guid.NewGuid().ToString();
+
+            await Assert.ThrowsAsync<MemphisSchemaValidationException>(
+                () => _sut.ValidateAsync(msg, nonexistentSchema));
+        }
+
+        #endregion
+    }
+}
---
+++ src/Memphis.Client.UnitTests/Validators/TestData/JsonValidatorTestData.cs
@@ -0,0 +1,195 @@
+using System.Text;
+
+namespace Memphis.Client.UnitTests.Validators.TestData;
+
+internal class JsonValidatorTestData
+{
+
+    public static IEnumerable<object[]> ValidSchema => new List<object[]>
+    {
+        new object[] { simpleSch },
+        new object[] { greetingSch },
+        new object[] { userInfoSch },
+    };
+
+    public static IEnumerable<object[]> InvalidSchema => new List<object[]>
+    {
+        new object[] { invalidSch1 },
+        new object[] { invalidSch2 },
+    };
+
+    public static IEnumerable<object[]> Message => new List<object[]>
+    {
+        new object[] { Encoding.UTF8.GetBytes(simpleMsg) },
+        new object[] { Encoding.UTF8.GetBytes(greetingMsg) },
+        new object[] { Encoding.UTF8.GetBytes(userInfoMsg) },
+    };
+
+    public static IEnumerable<object[]> ValidSchemaDetail => new List<object[]>
+    {
+        new object[] { "valid-simple-msg", simpleSch, Encoding.UTF8.GetBytes(simpleMsg) },
+        new object[] { "valid-greeting-msg", greetingSch, Encoding.UTF8.GetBytes(greetingMsg) },
+        new object[] { "valid-userinfo-msg", userInfoSch, Encoding.UTF8.GetBytes(userInfoMsg) },
+    };
+
+    public static IEnumerable<object[]> InvalidSchemaDetail => new List<object[]>
+    {
+        new object[] { "invalid-simple-msg", simpleSch, Encoding.UTF8.GetBytes(invalidMsg) },
+        new object[] { "invalid-greeting-msg", greetingSch, Encoding.UTF8.GetBytes(invalidMsg) },
+        new object[] { "invalid-userinfo-msg", userInfoSch, Encoding.UTF8.GetBytes(invalidMsg) },
+    };
+
+
+
+    private const string simpleSch = """
+        {
+            "$schema": "http://json-schema.org/draft-04/schema#",
+            "title": "SimpleMessage",
+            "type": "object",
+            "additionalProperties": false,
+            "required": [
+                "Message"
+            ],
+            "properties": {
+                "Message": {
+                "type": "string",
+                "minLength": 1
+                }
+            }
+        }
+    """;
+
+    private const string greetingSch = """
+        {
+            "$schema": "http://json-schema.org/draft-04/schema#",
+            "title": "Greeting",
+            "type": "object",
+            "additionalProperties": false,
+            "required": [
+                "From",
+                "Message"
+            ],
+            "properties": {
+                "From": {
+                "type": "string",
+                "minLength": 1
+                },
+                "Message": {
+                "type": "string",
+                "minLength": 1
+                }
+            }
+        }
+        """;
+
+    private const string userInfoSch = """
+    {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "title": "UserInfo",
+        "type": "object",
+        "additionalProperties": false,
+        "required": [
+            "FirstName",
+            "LastName"
+        ],
+        "properties": {
+            "FirstName": {
+            "type": "string",
+            "minLength": 1
+            },
+            "MiddleName": {
+            "type": [
+                "null",
+                "string"
+            ]
+            },
+            "LastName": {
+            "type": "string",
+            "minLength": 1
+            },
+            "Gender": {
+            "$ref": "#/definitions/Gender"
+            }
+        },
+        "definitions": {
+            "Gender": {
+            "type": "integer",
+            "description": "",
+            "x-enumNames": [
+                "Male",
+                "Female"
+            ],
+            "enum": [
+                0,
+                1
+            ]
+            }
+        }
+    }
+    """;
+
+    private const string invalidSch1 = """
+        {
+          "type": "object",
+          "properties": {
+            "name": {
+              "type": "string"
+            },
+            "age": {
+              "type": "integer"
+            },
+            "email": {
+              "type": "string",
+              "format": "email"
+            }
+          },
+          "required": ["name", "age"],
+          "additionalProperties": false
+          "invalid_key": "this is invalid because it is not a recognized keyword in JSON schema"
+        }
+        """;
+
+    private const string invalidSch2 = """
+        {
+            "type": "object",
+            "properties": {
+                "name": {
+                "type": "string"
+                }
+            },
+            "required": ["name", "age"],
+            "additionalProperties": false
+        }
+    """;
+
+    private const string simpleMsg = """
+        {
+            "Message":"Hello"
+        }
+        """;
+
+    private const string greetingMsg = """
+        {
+          "From": "Memphis",
+          "Message": "Welcome" 
+        }
+        """;
+
+    private const string userInfoMsg = """
+        {
+          "FirstName": "John",
+          "LastName": "Doe",
+          "Gender": 1  
+        }
+        """;
+
+    private const string invalidMsg = """
+        {
+          "MessageType":"Invalid"
+        }
+        """;
+
+
+}
+
+
