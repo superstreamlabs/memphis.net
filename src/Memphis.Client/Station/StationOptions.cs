b31ffef009a5800c177f7251b91aab2b9bda576f
Bazen <bazenteklehaymanot29@gmail.com>
2023-08-01 07:30:59 +0400

    Added AckBased retention type (#108)

--- src/Memphis.Client/Station/StationOptions.cs
+++ src/Memphis.Client/Station/StationOptions.cs
@@ -5,6 +5,18 @@ namespace Memphis.Client.Station
     public sealed class StationOptions
     {
         public string Name { get; set; }
+        /// <summary>
+        /// The retention type to use for the station. Valid values are:
+        /// <list type="bullet">
+        /// <item><description>message_age_sec</description></item>
+        /// <item><description>messages</description></item>
+        /// <item><description>bytes</description></item>
+        /// <item><description>ack_based</description></item>
+        /// </list>
+        /// <description>
+        /// Use the <see cref="RetentionTypes"/> class for the valid values.
+        /// </description>
+        /// </summary>
         public string RetentionType { get; set; } = RetentionTypes.MAX_MESSAGE_AGE_SECONDS;
         public int RetentionValue { get; set; } = 604_800;
         public string StorageType { get; set; } = StorageTypes.DISK;
@@ -21,6 +33,10 @@ namespace Memphis.Client.Station
         public const string MAX_MESSAGE_AGE_SECONDS = "message_age_sec";
         public const string MESSAGES = "messages";
         public const string BYTES = "bytes";
+        /// <summary>
+        /// Retention based on message acks. This is for cloud users only.
+        /// </summary>
+        public const string ACK_BASED = "ack_based";
     }
 
     public class StorageTypes
