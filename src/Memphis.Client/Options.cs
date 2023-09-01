660f1bcd4da2599e24363c012f372f6fdd5e2b99
Bazen <bazenteklehaymanot29@gmail.com>
2023-09-01 21:14:25 +0400

    Added close event hander, and default handler for MemphisClient (#135)

--- src/Memphis.Client/Options.cs
+++ src/Memphis.Client/Options.cs
@@ -1,43 +1,46 @@
+using System;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 
 #nullable disable 
 
-namespace Memphis.Client
+namespace Memphis.Client;
+
+public sealed class TlsOptions
 {
-    public sealed class TlsOptions
-    {
-        public TlsOptions(string fileName)
-            => (FileName) = (fileName);
+    public TlsOptions(string fileName)
+        => (FileName) = (fileName);
 
-        public TlsOptions(string fileName, string password) : this(fileName)
-            => (Password) = (password);
+    public TlsOptions(string fileName, string password) : this(fileName)
+        => (Password) = (password);
 
-        public TlsOptions(X509Certificate2 certificate)
-            => (Certificate) = (certificate);
+    public TlsOptions(X509Certificate2 certificate)
+        => (Certificate) = (certificate);
 
-        public X509Certificate2 Certificate { get; set; }
-        public string FileName { get; set; }
-        public string Password { get; set; }
-        public RemoteCertificateValidationCallback RemoteCertificateValidationCallback { get; set; }
-    }
+    public X509Certificate2 Certificate { get; set; }
+    public string FileName { get; set; }
+    public string Password { get; set; }
+    public RemoteCertificateValidationCallback RemoteCertificateValidationCallback { get; set; }
+}
 
-    public sealed class ClientOptions
-    {
-        public string Host { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string ConnectionToken { get; set; }
-        public int Port { get; set; }
-        public bool Reconnect { get; set; }
-        public int MaxReconnect { get; set; }
-        public int MaxReconnectIntervalMs { get; set; }
-        public int TimeoutMs { get; set; }
-        public TlsOptions Tls { get; set; }
+public sealed class ClientOptions
+{
+    public string Host { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string ConnectionToken { get; set; }
+    public int Port { get; set; }
+    public bool Reconnect { get; set; }
+    public int MaxReconnect { get; set; }
+    public int MaxReconnectIntervalMs { get; set; }
+    public int TimeoutMs { get; set; }
+    public TlsOptions Tls { get; set; }
 
-        /// <summary>
-        /// The AccountId field should be set only on the cloud version of Memphis, otherwise it will be ignored.
-        /// </summary>
-        public int AccountId { get; set; }
-    }
+    /// <summary>
+    /// The AccountId field should be set only on the cloud version of Memphis, otherwise it will be ignored.
+    /// </summary>
+    public int AccountId { get; set; }
+
+    public EventHandler<MemphisConnectionEventArgs> ClosedEventHandler;
+    
 }
