fb53135767c5a70c803ed38a93cf99d78df82fbc
Bazen <49089563+bazen-teklehaymanot@users.noreply.github.com>
2023-09-20 21:09:52 +0300

    Added DLS station test

--- src/Memphis.Client.IntegrationTests/Station/MemphisStationTests.cs
+++ src/Memphis.Client.IntegrationTests/Station/MemphisStationTests.cs
@@ -25,6 +25,23 @@ public class MemphisStationTests
     }
 
     [Theory]
+    [InlineData("station_with_dls_a", "dls_station_a")]
+    [InlineData("station_with_dls_b", "dls_station_b")]
+    public async Task GivenStationOptionsWithDls_WhenCreateStation_ThenStationWithDlsIsCreated(string stationName, string dlsStationName)
+    {
+        using var client = await MemphisClientFactory.CreateClient(_fixture.MemphisClientOptions);
+        var stationOptions = _fixture.DefaultStationOptions;
+        stationOptions.Name = stationName;
+        stationOptions.DlsStation = dlsStationName;
+
+        var result = await client.CreateStation(stationOptions);
+
+        await result.DestroyAsync();
+        Assert.NotNull(result);
+    }
+    
+
+    [Theory]
     [InlineData("station_tst_station_name_x")]
     [InlineData("station_tst_station_name_y")]
     public async Task GivenStationName_WhenCreateStation_ThenStationIsCreated(string stationName)
