fec26fcb807c385bfe6176187ae923f09ae76d02
John Peters <john@memphis.dev>
2024-01-08 23:00:58 -0600

    "Merged with master"

--- src/Memphis.Client/Exception/MemphisExceptions.cs
+++ src/Memphis.Client/Exception/MemphisExceptions.cs
@@ -2,33 +2,33 @@ namespace Memphis.Client;
 
 public static class MemphisExceptions{
 
-    public static readonly MemphisConnectionException DeadConnectionException = new MemphisConnectionException("Connection to the broker is dead");
+    public static readonly MemphisConnectionException DeadConnectionException = new("Connection to the broker is dead");
 
-    public static readonly MemphisConnectionException UnableToDealyDLSException = new MemphisConnectionException("Unable to delay DLS message");
+    public static readonly MemphisConnectionException UnableToDealyDLSException = new("Unable to delay DLS message");
 
-    public static readonly MemphisException EmptySchemaNameException = new MemphisException("Schema name can not be empty");
+    public static readonly MemphisException EmptySchemaNameException = new("Schema name can not be empty");
 
-    public static readonly MemphisException SchemaNameTooLongException = new MemphisException("Schema name should be under 128 characters");
+    public static readonly MemphisException SchemaNameTooLongException = new("Schema name should be under 128 characters");
 
-    public static readonly MemphisException InvalidSchemaNameException = new MemphisException("Only alphanumeric and the '_', '-', '.' characters are allowed in schema name");
+    public static readonly MemphisException InvalidSchemaNameException = new("Only alphanumeric and the '_', '-', '.' characters are allowed in schema name");
 
-    public static readonly MemphisException InvalidSchemaStartEndCharsException = new MemphisException("Schema name can not start or end with non alphanumeric character");
+    public static readonly MemphisException InvalidSchemaStartEndCharsException = new("Schema name can not start or end with non alphanumeric character");
 
-    public static readonly MemphisException EmptySchemaTypeException = new MemphisException("Schema type can not be empty");
+    public static readonly MemphisException EmptySchemaTypeException = new("Schema type can not be empty");
 
-    public static readonly MemphisException UnsupportedSchemaTypeException = new MemphisException("Unsupported schema type, the supported schema types are: json, graphql, protobuf, avro\"");
+    public static readonly MemphisException UnsupportedSchemaTypeException = new("Unsupported schema type, the supported schema types are: json, graphql, protobuf, avro\"");
 
-    public static readonly MemphisException SchemaUpdateSubscriptionFailedException = new MemphisException("Unable to add subscription of schema updates for station");
+    public static readonly MemphisException SchemaUpdateSubscriptionFailedException = new("Unable to add subscription of schema updates for station");
 
-    public static readonly MemphisException InvalidConnectionTypeException = new MemphisException("You have to connect with one of the following methods: connection token / password");
+    public static readonly MemphisException InvalidConnectionTypeException = new("You have to connect with one of the following methods: connection token / password");
 
-    public static readonly MemphisException StationUnreachableException = new MemphisException("Station unreachable");
+    public static readonly MemphisException StationUnreachableException = new("Station unreachable");
 
-    public static readonly MemphisException BothPartitionNumAndKeyException = new MemphisException("PartitionKey and PartitionNumber can not be set at the same time");
+    public static readonly MemphisException BothPartitionNumAndKeyException = new("PartitionKey and PartitionNumber can not be set at the same time");
 
-    public static readonly MemphisMessageIdException EmptyMessageIDException = new MemphisMessageIdException("Message ID cannot be empty");
+    public static readonly MemphisMessageIdException EmptyMessageIDException = new("Message ID cannot be empty");
 
-    public static readonly MemphisException UnsupportedOSException = new MemphisException("Unsupported OS");
+    public static readonly MemphisException UnsupportedOSException = new("Unsupported OS");
 
     public static MemphisException FailedToConnectException(System.Exception e)
     {
