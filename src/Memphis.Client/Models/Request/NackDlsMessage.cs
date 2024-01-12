9a13ae39d54158ae496247536e537782dbc85e8a
Bazen <49089563+bazen-teklehaymanot@users.noreply.github.com>
2024-01-12 10:56:08 +0200

    Added Nack

---
+++ src/Memphis.Client/Models/Request/NackDlsMessage.cs
@@ -0,0 +1,21 @@
+﻿namespace Memphis.Client;
+
+[DataContract]
+internal class NackDlsMessage
+{
+    [DataMember(Name = "station_name")]
+    public string StationName { get; set; } = null!;
+
+    [DataMember(Name = "error")]
+    public string Error { get; set; } = null!;
+    
+    [DataMember(Name = "partition")]
+    public int Partition { get; set; }
+    
+    [DataMember(Name = "cg_name")]
+    public string ConsumerGroupName { get; set; } = null!;
+
+    [DataMember(Name = "seq")]
+    public ulong StreamSequence { get; set; }
+    
+}
