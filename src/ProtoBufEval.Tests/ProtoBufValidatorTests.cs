ac0c0249f5438455ab1819a158156f75b380358e
Bazen <49089563+bazen-teklehaymanot@users.noreply.github.com>
2024-01-29 10:58:56 +0200

    updated tests

--- src/ProtoBufEval.Tests/ProtoBufValidatorTests.cs
+++ src/ProtoBufEval.Tests/ProtoBufValidatorTests.cs
@@ -47,6 +47,9 @@ public class InvalidModel
 
     [ProtoMember(2, Name = @"field2")]
     public string Field2 { get; set; } = "";
+    [ProtoMember(3, Name = @"field3")]
+    public string Field3 { get; set; } = "";
+
 }
 
 
@@ -72,7 +75,7 @@ public class ProtoBufValidatorTests
             Field2 = "SecondField",
             Field3 = 333,
         };
-        var base64ValidData = ConvertProtoBufToBase64(validData);
+        var base64ValidData = Proto64(validData);
         var activeSchemaVersionBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(activeSchemaVersion));
 
         var result = await ProtoBufValidator.Validate(base64ValidData, activeSchemaVersionBase64, schemaName);
@@ -109,7 +112,7 @@ public class ProtoBufValidatorTests
     [Fact]
     public async Task GivenInvalidData_WhenValidate_ThenHasError()
     {
-        var base64InvalidData = ConvertProtoBufToBase64(new InvalidModel
+        var base64InvalidData = Proto64(new InvalidModel
         {
             Field1 = "AwesomeFirst",
             Field2 = "SecondField"
@@ -124,7 +127,7 @@ public class ProtoBufValidatorTests
     [Fact]
     public async Task GivenValidData_WhenValidate_ThenHasError()
     {
-        var base64InvalidData = ConvertProtoBufToBase64(new ValidModel
+        var base64InvalidData = Proto64(new ValidModel
         {
             Field1 = "AwesomeFirst",
             Field2 = "SecondField",
@@ -137,11 +140,50 @@ public class ProtoBufValidatorTests
         Assert.False(result.HasError);
     }
 
-    private static string ConvertProtoBufToBase64<TData>(TData obj) where TData : class
+
+    [Fact]
+    public async Task GivenInvalidData_WhenValidate3_ThenHasError()
+    {
+        var base64InvalidData = Json64(new InvalidModel
+        {
+            Field1 = "AwesomeFirst",
+            Field2 = "SecondField",
+            Field3 = "WrongData",
+        });
+
+        var activeSchemaVersionBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(ActiveVersion3()));
+
+        var result = await ProtoBufValidator.ValidateJson(base64InvalidData, activeSchemaVersionBase64, "testschema");
+
+        Assert.True(result.HasError);
+    }
+
+    [Fact]
+    public async Task GivenValidData_WhenValidate3_ThenHasNoError()
+    {
+        var base64InvalidData = Json64(new ValidModel
+        {
+            Field1 = "AwesomeFirst",
+            Field2 = "SecondField",
+            Field3 = 333,
+        });
+        var activeSchemaVersionBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(ActiveVersion3()));
+
+        var result = await ProtoBufValidator.ValidateJson(base64InvalidData, activeSchemaVersionBase64, "testschema");
+
+        Assert.False(result.HasError);
+    }
+
+    private static string Proto64<TData>(TData obj) where TData : class
     {
         using var stream = new MemoryStream();
-        ProtoBuf.Serializer.Serialize(stream, obj);
+        Serializer.Serialize(stream, obj);
         return Convert.ToBase64String(stream.ToArray());
+    }
+
+    private static string Json64<TData>(TData obj) where TData : class
+    {
+        return Convert.ToBase64String(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj)));
     }
 
     private static string ActiveVersion()
@@ -151,6 +193,17 @@ public class ProtoBufValidatorTests
             version_number = 1,
             descriptor = "CmQKEnRlc3RzY2hlbWFfMS5wcm90byJOCgRUZXN0EhYKBmZpZWxkMRgBIAIoCVIGZmllbGQxEhYKBmZpZWxkMhgCIAIoCVIGZmllbGQyEhYKBmZpZWxkMxgDIAIoBVIGZmllbGQz",
             schema_content = "syntax = \"proto2\";\nmessage Test {\n            required string field1 = 1;\n            required string field2 = 2;\n            required int32 field3 = 3;\n}",
+            message_struct_name = "Test"
+        });
+    }
+
+    private static string ActiveVersion3()
+    {
+        return JsonConvert.SerializeObject(new
+        {
+            version_number = 1,
+            descriptor = "CmwKEnRlc3RzY2hlbWFfMS5wcm90byJOCgRUZXN0EhYKBmZpZWxkMRgBIAEoCVIGZmllbGQxEhYKBmZpZWxkMhgCIAEoCVIGZmllbGQyEhYKBmZpZWxkMxgDIAEoBVIGZmllbGQzYgZwcm90bzM=",
+            schema_content = "syntax = \"proto3\";\nmessage Test {\n    string field1 = 1;\n    string  field2 = 2;\n    int32  field3 = 3;\n}",
             message_struct_name = "Test"
         });
     }
